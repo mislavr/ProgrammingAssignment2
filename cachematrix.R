104706f88cd3b6bff72a3ceafe884e54553765e4
Mislav Ruzic <mislav@redirekt.net>
2014-12-13 15:54:54 +0100

    final solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## stores and retrieves original matrix and an inverse of a matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(a = matrix()) {
+    ## parameters:
+    ## a - matrix
+    ## result:
+    ## list of 4 getter/setter functions - list(set, get, setInverse, getInverse)
+  
+    ## reset inverse value on function call
+    inverseA <- NULL
+  
+    ## set original matrix value
+    set <- function(b) {
+      a <<- b
+      inverseA <<- NULL
+    }
+    
+    ## get original matrix value
+    get <- function() a
+    
+    ## set inverse matrix value
+    setInverse <- function(inverse) inverseA <<- inverse
+    
+    ## get inverse matrix value
+    getInverse <- function() inverseA
+    
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## calculates inverse of a matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(a) {
+    ## parameters:
+    ## a - list returned by function makeCacheMatrix
+    ## result:
+    ## matrix - inverse of matrix a$get() 
+      
+    ## check if inverse of a matrix is already calculated/cached
+  
+    inverse <- a$getInverse()
+    if(!is.null(inverse)) {
+      message("getting cached data")
+      return(inverse)
+    }
+    
+    ## check if matrix is singular (non invertible) by calculating determinant (det)
+    ## linear algebra fact: matrix A is singular iff det(A)=0
+    
+    if (det(a$get())==0) stop("matrix is singular")
+    
+    ## calculate and store inverse of matrix
+    
+    inverse <- solve(a$get())
+    a$setInverse(inverse)
+    inverse
 }
